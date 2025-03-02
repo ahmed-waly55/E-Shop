11d077a882605416c483497f8f2553b860d243e3
ahmed waly <ahmedwaly074@gmail.com>
2025-03-02 23:03:55 +0200

    update Desgin and Structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'auth',loadComponent:()=> import('./layouts/auth-layout/auth-layout.component').then(
+        (c) => c.AuthLayoutComponent
+    ) ,
+    children:[
+        {path:'',redirectTo:'login',pathMatch:'full'},
+        {
+            path:'login',loadComponent:()=> import('./pages/login/login.component').then(
+            (c) => c.LoginComponent)
+        },
+        {
+            path:'register',loadComponent:()=> import('./pages/register/register.component').then(
+                (c) => c.RegisterComponent)
+        },
+    ]
+},
+
+{
+    path:'user' , loadComponent:()=> import('./layouts/user-layout/user-layout.component').then(
+        (c) => c.UserLayoutComponent
+    ),
+},
+
+];
