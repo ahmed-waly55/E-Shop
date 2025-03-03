1ec4330a9c677408289fcf1342a1aaaeb1aead52
ahmed waly <ahmedwaly074@gmail.com>
2025-03-03 02:22:27 +0200

    added register desgin

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,64 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, InputGroupModule, InputGroupAddonModule, InputTextModule,ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  name!:FormControl
+  email!:FormControl
+  password!:FormControl
+  rePassword!:FormControl
+
+  registrationForm!:FormGroup
+
+  constructor(){
+    this.initFormControls();
+    this.initFormGroupe();
+
+  }
+
+  initFormControls(): void {
+    this.name = new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]);
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]);
+    this.rePassword = new FormControl('', [
+      Validators.required,
+      this.passwordMatch(this.password),
+    ]);
+  }
+
+  initFormGroupe(): void {
+    this.registrationForm = new FormGroup({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      rePassword: this.rePassword,
+    });
+  }
+
+  passwordMatch(pass: AbstractControl): ValidatorFn {
+    return (rePass: AbstractControl): null | { [key: string]: boolean } => {
+      if (pass.value !== rePass.value || rePass.value === '') {
+        return { passNotMatch: true };
+      } else return null;
+    };
+  }
 
 }
